b484c583f5bd312cfdab22cc62793cc5c2b02500
Harshithakm08 <kmharshitha2004@gmail.com>
2025-12-14 17:27:17 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import path from "path"
 import cors from "cors";
 import {serve} from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
 
 import {ENV} from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import {inngest, functions} from "./lib/inngest.js";
+import chatRoutes from "./routes/chatRoutes.js";
+
 
 const app = express();
 
@@ -15,8 +18,10 @@ const __dirname = path.resolve();
 app.use(express.json());
 //credentials true meaning?? ==>server allows browser to include cookies on requests 
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
+app.use(clerkMiddleware())//this has auth  field to request object: req.auth()
 
 app.use("/api/inngest", serve({client:inngest,functions}));
+app.use("/api/chat", chatRoutes)
 
 app.get("/health", (req,res)=>{
     res.status(200).json({
@@ -24,11 +29,18 @@ app.get("/health", (req,res)=>{
     });
 });
 
-app.get("/books", (req,res)=>{
-    res.status(200).json({
-        msg:"this is the books endpoints"
-    });
-});
+// app.get("/books", (req,res)=>{
+//     res.status(200).json({
+//         msg:"this is the books endpoints"
+//     });
+// });
+
+//when you pass an array of middleware to express, it automatically flattens and executes them sequentially, one by one
+
+// app.get("/video-calls", protectRoute,(req,res) =>{
+//     res.status(200).json({msg:"this is a protested route"});
+
+// });
 
 //make our app ready for deployment
 if(ENV.NODE_ENV === "production"){
